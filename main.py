a5a8cde62f5a25c3b5dc9187046ba547cbf02be0
fbrandaoq <fbrandaoq@gmail.com>
2024-05-24 20:17:43 -0300

    Versão 1.1

--- main.py
+++ main.py
@@ -3,20 +3,16 @@ tarefas = []
 
 while True:
     # usuário informa o nome da nova tarefa
-    nova_tarefa = input('Informe o nome da nova tarefa: ')
-
-    # nova tarefa é inserida na lista
-    tarefas.append(nova_tarefa)
+    nova_tarefa = input('Informe anova tarefa ou tecle Enter para encerrar e exibir a lista: ')
     
-    # pergunta se quer continuar a inserção de tarefas
-    retorno = input('Deseja cadastrar nova tarefa?(s/n)')
-
-    # se a resposta for Sim, então adiciona novamente
-    if retorno == 's':
+  # se a resposta for Sim, então adiciona novamente
+    if nova_tarefa != '':
+        # nova tarefa é inserida na lista
+        tarefas.append(nova_tarefa)
         continue
     else: # senão, imprima as tarefas
         #print(tarefas)
         break
-
+# exibe a lista de tarefas
 for tarefa in tarefas:
     print(tarefa)
